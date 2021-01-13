62746f610910bbaa5b28409759d9462b63a0870b
AdamsParker <adamssfd@gmail.com>
2021-01-13 03:55:07 -0800

    updated UI, file conflicts is no longer a problem

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const guideList = document.querySelector('.content__container')
+const workspaceLink = document.querySelectorAll('#workspace');
 const loggedOutLinks = document.querySelectorAll('#logout');
 const loggedInLInks = document.querySelectorAll('#login');
 const accountDetails = document.querySelector('.account-details');
@@ -17,13 +18,14 @@ const setupUI = (user) => {
           //hide account info
           accountDetails.innerHTML = '';
 
+        workspaceLink.forEach(item => item.style.display = 'block');
         createLink.forEach(item => item.style.display = 'block');
         loggedInLInks.forEach(item => item.style.display = 'none');
         loggedOutLinks.forEach(item => item.style.display = 'block');
        
     }
     else {
-     
+        workspaceLink.forEach(item => item.style.display = 'none');
         createLink.forEach(item => item.style.display = 'none');
         loggedInLInks.forEach(item => item.style.display = 'block');
         loggedOutLinks.forEach(item => item.style.display = 'none');
